922e3b1644616914ae788490449ae1f57ed6ddba
Dan LaManna <me@danlamanna.com>
2015-08-22 21:27:55 -0400

    Update commentary

--- helm-ctest.el
+++ helm-ctest.el
@@ -1,4 +1,22 @@
 ;;; helm-ctest.el --- Run ctest from within emacs
+
+;; Copyright (C) 2015 Dan LaManna
+
+;; Author: Dan LaManna <me@danlamanna.com>
+;; Version: 1.0
+;; Keywords: helm,ctest
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, version 2 of the License.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 (require 's)
 (require 'dash)
 
